56087a376f3cd50777ccccaa91ebd8170959f296
pz771114 <54385561+pz771114@users.noreply.github.com>
2021-07-27 01:14:44 -0400

    render frontpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,43 @@
-import logo from './logo.svg';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-import Navigation from './components/layout/Navigation';
-import Header from './components/layout/Header';
 import Content from './components/layout/Content';
 import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Navigation from './components/layout/Navigation';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import axios from 'axios';
+import {useEffect,useState} from 'react';
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    async function getProducts()
+    {
+      const result = await axios.get('/api/v1/products');
+      setProducts(result.data.products);
+    }
+
+    getProducts();
+    
+      
+  }, []);
+
+  
   return (
-    <div className="App">
-      <Navigation/>
-      <Header/>
-      <Content />
-      <Footer/>
-    </div>
+   <Router>
+     <Navigation/>
+     <Header/>
+     <Route path="/" exact>
+       <Content products={products}/>
+     </Route>
+     
+     <Footer/>
+   </Router>
+   
   );
 }
 
