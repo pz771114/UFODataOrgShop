3b532fef0daa739f7515c3e6992f03f2fc5110ca
pz771114 <54385561+pz771114@users.noreply.github.com>
2021-08-05 14:10:44 -0400

    Create Redux store

--- frontend/src/components/layout/Content.js
+++ frontend/src/components/layout/Content.js
@@ -8,7 +8,7 @@ const Content = (props) => {
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                   {products.map(product=><Product product={product}/>)}
+                   {products.map(product=><Product product={product} key={product._id}/>)}
                    
                     
                 </div>
