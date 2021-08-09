ccd89bbbe2cb997b254d9df2c41f1dcd4c849604
pz771114 <54385561+pz771114@users.noreply.github.com>
2021-08-08 23:51:51 -0400

    Add React loader spinner

--- frontend/src/components/layout/Content.js
+++ frontend/src/components/layout/Content.js
@@ -3,25 +3,29 @@ import Product from "../product";
 import {useDispatch, useSelector} from 'react-redux';
 import {getProducts} from '../../actions/productsAction';
 import { useEffect } from "react";
+import LoaderSpinner from "./LoaderSpinner";
 
+import {useAlert} from 'react-alert';
 const Content = () => {
     
-    const {loading, products, productsCount} = useSelector(state=>state.products);
-
+    const {loading, products, productsCount,error} = useSelector(state=>state.products);
     const dispatch = useDispatch();
-
+    const alert = useAlert();
     useEffect(()=>{
         dispatch(getProducts());
-
-    },[dispatch]);
+        if(error)
+        {
+            alert.error(error);
+        }
+    },[dispatch,error]);
    
+    const productsDiv = products.map(product=><Product product={product} key={product._id}/>);
     return (
         <section className="py-5">
+            
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                   {products.map(product=><Product product={product} key={product._id}/>)}
-                   
-                    
+                    {loading ?<LoaderSpinner />:productsDiv}                    
                 </div>
             </div>
         </section>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,27 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import store from './store/store';
 
+//Alert
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
+    
   </Provider>,
   document.getElementById('root')
 );
