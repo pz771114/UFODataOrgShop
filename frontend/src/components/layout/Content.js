5c98915954726bc091e5c68619311faa4e6218f2
pz771114 <54385561+pz771114@users.noreply.github.com>
2021-08-12 00:19:43 -0400

    Manage metadat using react-helmet

--- frontend/src/components/layout/Content.js
+++ frontend/src/components/layout/Content.js
@@ -5,6 +5,7 @@ import { getProducts } from "../../actions/productsAction";
 import { useEffect } from "react";
 import LoaderSpinner from "./LoaderSpinner";
 import { toast } from "react-toastify";
+import { Helmet } from "react-helmet";
 
 const Content = () => {
   const { loading, products, productsCount, error } = useSelector(
@@ -27,6 +28,14 @@ const Content = () => {
     : "";
   return (
     <section className="py-5">
+      <Helmet>
+        <title>Shop - UFODATA</title>
+        <meta
+          name="description"
+          content="Best UFO Alien accessories - UFODATA"
+        />
+      </Helmet>
+
       <div className="container px-4 px-lg-5 mt-5">
         <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
           {loading ? <LoaderSpinner /> : productsDiv}
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,14 +1,26 @@
 const Header = () => {
-    return (
-        <header className="bg-dark py-5">
-            <div className="container px-4 px-lg-5 my-5">
-                <div className="text-center text-white">
-                    <h1 className="display-4 fw-bolder">Shop in style</h1>
-                    <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
-                </div>
-            </div>
-        </header>
-     );
-}
- 
+  return (
+    <header className="bg-dark py-5">
+      <div className="container px-4 px-lg-5 my-5">
+        <div className="text-center text-white">
+          <h1 className="display-4 fw-bolder">UFODATA Shop</h1>
+          <p className="lead fw-normal text-white-50 mb-0">
+            Best UFO Alien accessories
+          </p>
+        </div>
+
+        <div className="overflow-hidden" style={{ maxHeight: "30vh" }}>
+          <div className="container px-5">
+            <img
+              src="hero.jpg"
+              className="img-fluid border rounded-3 shadow-lg mb-4"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
 export default Header;
