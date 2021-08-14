078a2c0ac2eea5ed3a1f6ffa69cb3dae1723e075
pz771114 <54385561+pz771114@users.noreply.github.com>
2021-08-13 22:42:02 -0400

    Create single product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,36 +1,48 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.createProduct = async (req, res, next) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({
-            success: true,
-            product
-        })
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            error: error.message
-        })
-    }
-
-}
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
 exports.getProducts = async (req, res, next) => {
-    
-    try {
-       
-        const products = await Product.find();
-        res.status(200).json({
-            success: true,
-            products,
-            productsCount: products.length
-        })
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            error: error.message
-        })
-    }
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      success: true,
+      products,
+      productsCount: products.length,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
 
-}
+exports.getSingleProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: "Product ID not found",
+    });
+  }
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { getProducts, createProduct } = require('../controllers/productController');
-router.route('/products').get(getProducts);
-router.route('/product/new').post(createProduct);
+const {
+  getProducts,
+  createProduct,
+  getSingleProduct,
+} = require("../controllers/productController");
+router.route("/products").get(getProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").get(getSingleProduct);
 
 module.exports = router;
